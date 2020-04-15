8172b4dffea3c872f895a98bb9490f4cd75fe077
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-04-15 13:12:57 -0700

    Plots now working!

--- StarterCode/static/app.js
+++ StarterCode/static/app.js
@@ -1,72 +1,83 @@
 // 1. Use the D3 library to read in samples.json.
 function readJson(){
-    d3.json("../samples.json").then(function(data) {
-        var biodivData = unpack(resolved.dataset.data[0]);
-        var trace1 = {
-            values: sample_values,
-            labels: otu_ids,
-            hovertext: otu_labels,
-            type: "bar",
-            orientation: "h"
-        };
+    d3.json("../data/samples.json").then(function(data) {
+        var names = data.names;
+        var select = d3.select("#selDataset");
+        names.forEach(function (data) {
+            select.append("option").text(data).property("value", data);
+        })
+        createChart(names[0]);
     })
 };
 
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-var layout = {
-    title: "Top 10 OTUs",
-    xaxis: {
-        title: "Microbial population"
-    },
-    yaxis: {
-        title: "OTU id"
-    }
-};
+readJson();
 
-d3.select("#bar")
+// Function to create charts
+function createChart(sample) {
+    d3.json("../data/samples.json").then(function(data) {
+        var result = data.samples.filter(obj => obj.id == sample)[0];
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
 
-Plotly.newPlot("plot", trace1, layout);
+        // 3. Create a bubble chart that displays each sample.
+        var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        mode: 'markers',
+        marker: {
+            size: sample_values,
+            color: otu_ids
+        }
+        };
 
-// 3. Create a bubble chart that displays each sample.
-var trace1 = {
-    x: otu_ids,
-    y: sample_values,
-    mode: 'markers',
-    marker: {
-      size: sample_values
-    }
-  };
-  
-  var data = [trace1];
-  
-  var layout = {
-    title: 'Total numbers, by OTU',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
+        var data = [trace1];
 
-  d3.select("#bubble")
+        var layout = {
+        title: 'Total numbers, by OTU',
+        showlegend: false,
+        height: 600,
+        width: 1200
+        };
 
-  Plotly.newPlot('myDiv', data, layout);
+        Plotly.newPlot('bubble', data, layout);
 
-// 4. Display the sample metadata, i.e., an individual's demographic information
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+        // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        var trace2 = [{
+            x: sample_values.slice(0, 10).reverse(),
+            y: otu_ids.slice(0, 10).map(otu_ids => `OTU ${otu_ids}`).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+            }];
 
-function unpack(rows, index){
-    return rows.map(function(row){
-        return row[index];
-    });
+        var layout = {
+            title: "Top 10 OTUs",
+            xaxis: {
+                title: "Microbial population"
+            },
+            yaxis: {
+                title: "OTU id"
+            }
+        };
+        
+        Plotly.newPlot("bar", trace2, layout);
+    })
+        
 }
 
-d3.select("#sample-metadata").append(row[index]);
+// // 4. Display the sample metadata, i.e., an individual's demographic information
+// // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+// function unpack(rows, index){
+//     return rows.map(function(row){
+//         return row[index];
+//     });
+// }
+
+// d3.select("#sample-metadata").append(row[index]);
 
 // 6. Update all of the plots any time that a new sample is selected.
-d3.select("#selDataset").on("change", updateChanges);
-function updateChanges() {
-    var dropdown = d3.select("#selDataset");
-    var dataset = dropdown.node.value;
-    // Put if statements here
-    Plotly.restyle("plot", "x", [x]);
-    Plotly.restyle("myDiv", "y", [y]);
+function optionChanged(newSample) {
+    createChart(newSample);
 }
